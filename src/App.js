87f6b1b03833154ded2e787b2bf6f9f773d6b131
sharath-98 <sharathchander.p@gmail.com>
2022-10-19 20:35:38 -0500

    Navbar initial design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
+      <Board/>
+      <Keyboard/>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <h2>Wordle</h2>
+    </div>
+  )
+}
+
+export default Nav
