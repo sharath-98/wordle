2b03d2060e5ce4f74224d0cb3edd97ad2e8f117e
sharath-98 <sharathchander.p@gmail.com>
2022-10-20 14:43:20 -0500

    randomized word selection

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDqMbLYami74fNQq_ATnT6J-A7xCx8FYyw",
+  authDomain: "wordle-834b4.firebaseapp.com",
+  projectId: "wordle-834b4",
+  storageBucket: "wordle-834b4.appspot.com",
+  messagingSenderId: "701650952254",
+  appId: "1:701650952254:web:588e6fdf2ef6fe9b68716f",
+  measurementId: "G-VM1PPS1REB"
+};
